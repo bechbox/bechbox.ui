626e4f039bab3e75b93e02ccee6f36ba0eca6416
Mikkel Bech <mikkel@bechbox.dk>
2024-01-07 12:12:25 +0100

    Update vite.config.js
    Add react plugin and configure output for CSS files

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
-//vite.config.js
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
       name: "bechbox.ui",
-
       fileName: "index",
     },
     rollupOptions: {
       external: ["react"],
+      output: {
+        // Ensure CSS is output as a separate file
+        assetFileNames: (assetInfo) =>
+          assetInfo.name.endsWith(".css") ? "style.css" : assetInfo.name,
+      },
     },
   },
 });
